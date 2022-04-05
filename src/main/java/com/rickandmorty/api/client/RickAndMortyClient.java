370618a8ff5d83ef83f2726a14202ce887a8b388
Geovani Ferreira <gugaheadbanger@gmail.com>
2022-04-05 15:50:40 -0300

    get de personagens por id

--- src/main/java/com/rickandmorty/api/client/RickAndMortyClient.java
+++ src/main/java/com/rickandmorty/api/client/RickAndMortyClient.java
@@ -4,9 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.rickandmorty.api.response.CharacterResponse;
+
+import ch.qos.logback.classic.Logger;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import org.slf4j.LoggerFactory;
 
 @Service
 @Slf4j
@@ -19,6 +24,7 @@ public class RickAndMortyClient {
 	}
 
 	public Mono<CharacterResponse> findCharacterById(String id) {
+		org.slf4j.Logger log = LoggerFactory.getLogger(RickAndMortyClient.class);
 		log.info("Buscando personagem de id [{}]", id);
 		return webClient.get().uri("/character/" + id).accept(APPLICATION_JSON).retrieve()
 				.onStatus(HttpStatus::is4xxClientError,
